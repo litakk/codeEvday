0f668b3abbfce0baaed428194cc8b17f66a98450
Feruz <140828663+litakk@users.noreply.github.com>
2025-01-24 10:00:16 +0500

    update 1

---
+++ DOM/index.js
@@ -0,0 +1,13 @@
+// BOM
+
+// Alert
+alert("Привет!");
+
+// Confirm
+const confirmResult = confirm("Хочешь научиться верстать?");
+console.log(confirmResult);
+
+// Prompt
+const promptResult = prompt("Кто ты по жизни?")
+console.log(promptResult)
+
