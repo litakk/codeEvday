2afb166a85b43519e2138fc4c947c7beabd8471e
litakk <aazizaliev2403@gmail.com>
2025-06-08 23:56:51 +0500

    upd

--- LeetCode/main.js
+++ LeetCode/main.js
@@ -124,3 +124,17 @@ function uniqueValues(arr) {
 uniqueValues([]); // []
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+function Acc(arr) {
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+
+  return result;
+}
+
+console.log(Acc([1, 2, 3])); // → 6
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
