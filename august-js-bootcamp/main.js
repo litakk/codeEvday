daed04528b374004b4dd6b895b3ca6a2a49d11f8
litakk <aazizaliev2403@gmail.com>
2025-08-01 16:05:52 +0500

    upd

---
+++ august-js-bootcamp/main.js
@@ -0,0 +1,29 @@
+// js - делит типы данных на 2 вида, примитивы и объекты
+
+// примитивы - string, number, null, boolean, undefined, bigInt, symbol
+
+// объекты - object, array, function
+
+// string + number = string
+// number + string = string
+
+//  при + будет = string
+//  в остальных случаях будет = number
+
+//  - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// undefined + 1 = NaN
+// null + 1 // 1
+// null == undefined // true
+// null === undefined // false
+
+//  - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// let obj1 = { name: "Alice" };
+// let obj2 = obj1;
+// obj2.name = "Bob";
+// console.log(obj1.name);
+
+//  - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
