878df68ccc7843065c6a688b7c702c19c9440464
litakk <aazizaliev2403@gmail.com>
2025-09-24 19:40:43 +0500

    group anagrams

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -6,13 +6,13 @@ var groupAnagrams = function (strs) {
   let seen = new Map();
 
   for (let str of strs) {
-    let key = str.split("").sort().join("")
-    if(!seen.has(key)) {
-        seen.set(key, [])
+    let key = str.split("").sort().join("");
+    if (!seen.has(key)) {
+      seen.set(key, []);
     }
-    seen.get(key).push(str)
+    seen.get(key).push(str);
   }
 
-  return Array.from(seen.values())
+  return Array.from(seen.values());
 };
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
