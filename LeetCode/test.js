2664c3ce38ca42d5237ec68be58b6cc4e45a8529
litakk <aazizaliev2403@gmail.com>
2025-10-14 18:13:09 +0500

    upd

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,27 +1,33 @@
-var nextGreaterElement = function(nums1, nums2) {
-  let result = []
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var romanToInt = function (s) {
+   const map = {
+     I: 1,
+     V: 5,
+     X: 10,
+     L: 50,
+     C: 100,
+     D: 500,
+     M: 1000,
+   };
 
-  for (let num of nums1) {
-    // 1️⃣ ищем индекс этого числа в nums2
-    let index = nums2.indexOf(num)
+   let total = 0;
 
-    // 2️⃣ флаг — нашли ли большее
-    let found = -1
+   for (let i = 0; i < s.length; i++) {
+     const current = map[s[i]];
+     const next = map[s[i + 1]];
 
-    // 3️⃣ идём вправо от позиции index
-    for (let j = index + 1; j < nums2.length; j++) {
-      if (nums2[j] > num) {
-        found = nums2[j] // нашли первое большее
-        break // сразу выходим
-      }
-    }
+     if (current < next) {
+       total -= current;
+     } else {
+       total += current;
+     }
+   }
 
-    // 4️⃣ добавляем найденное значение (или -1)
-    result.push(found)
-  }
-
-  return result
-}
-
-console.log(nextGreaterElement([4,1,2], [1,3,4,2])) // [-1, 3, -1]
-console.log(nextGreaterElement([2,4], [1,2,3,4]))   // [3, -1]
+   return total;
+ };
+ console.log(romanToInt("III"));
+ console.log(romanToInt("LVIII"));
+ console.log(romanToInt("MCMXCIV"));
