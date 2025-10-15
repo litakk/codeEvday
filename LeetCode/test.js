76606f6fcbdd69fc25735fecd6c1ead365feaed4
litakk <aazizaliev2403@gmail.com>
2025-10-15 18:52:59 +0500

    upd

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,33 +1,43 @@
 /**
- * @param {string} s
- * @return {number}
+ * @param {string[]} strs
+ * @return {string}
  */
- var romanToInt = function (s) {
-   const map = {
-     I: 1,
-     V: 5,
-     X: 10,
-     L: 50,
-     C: 100,
-     D: 500,
-     M: 1000,
-   };
+var longestCommonPrefix = function (strs) {
+  let prefix = strs[0];
 
-   let total = 0;
+  for (let i = 0; i < strs.length; i++) {
+    while (!strs[i].startsWith(prefix)) {
+      prefix = prefix.slice(0, -1);
+    }
+  }
+  return prefix;
+};
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
 
-   for (let i = 0; i < s.length; i++) {
-     const current = map[s[i]];
-     const next = map[s[i + 1]];
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
 
-     if (current < next) {
-       total -= current;
-     } else {
-       total += current;
-     }
-   }
+// 14. Longest Common Prefix
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Write a function to find the longest common prefix string amongst an array of strings.
 
-   return total;
- };
- console.log(romanToInt("III"));
- console.log(romanToInt("LVIII"));
- console.log(romanToInt("MCMXCIV"));
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+// Constraints:
+
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters if it is non-empty.
