bf9fb34a9cd65c41f4e80423a8050927efbcca26
litakk <aazizaliev2403@gmail.com>
2025-10-25 14:47:01 +0500

    upd

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,43 +1,23 @@
 /**
- * @param {string[]} strs
- * @return {string}
+ * @param {string} s
+ * @return {boolean}
  */
-var longestCommonPrefix = function (strs) {
-  let prefix = strs[0];
+var isPalindrome = function (s) {
+  // "A man, a plan, a canal: Panama"
+  let filter1 = s.toLowerCase().trim(); // a man, a plan, a canal: panama
+  let res = "";
+  for (let i = 0; i < filter1.length; i++) {
+    let char = filter1[i];
 
-  for (let i = 0; i < strs.length; i++) {
-    while (!strs[i].startsWith(prefix)) {
-      prefix = prefix.slice(0, -1);
+    if ((char >= "a" && char <= "z") || (char >= "0" && char <= "9")) {
+      res += char;
     }
   }
-  return prefix;
+
+  if (res === res.split("").reverse().join("")) {
+    return true;
+  } else return false;
 };
-console.log(longestCommonPrefix(["flower", "flow", "flight"]));
 
-// Input: strs = ["flower","flow","flight"]
-// Output: "fl"
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
 
-// 14. Longest Common Prefix
-// Easy
-// Topics
-// premium lock icon
-// Companies
-// Write a function to find the longest common prefix string amongst an array of strings.
-
-// If there is no common prefix, return an empty string "".
-
-// Example 1:
-
-// Input: strs = ["flower","flow","flight"]
-// Output: "fl"
-// Example 2:
-
-// Input: strs = ["dog","racecar","car"]
-// Output: ""
-// Explanation: There is no common prefix among the input strings.
-
-// Constraints:
-
-// 1 <= strs.length <= 200
-// 0 <= strs[i].length <= 200
-// strs[i] consists of only lowercase English letters if it is non-empty.
