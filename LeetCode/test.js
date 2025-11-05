9f8a0dea55a1edb3789ff4a246427574833a56db
litakk <aazizaliev2403@gmail.com>
2025-11-05 15:03:28 +0500

    update

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,17 +1,12 @@
-/**
- * @param {number} num
- * @return {number}
- */
-var addDigits = function (num) {
-  while (num >= 10) {
-    num = num
-      .toString()
-      .split("")
-      .reduce((sum, digit) => sum + +digit, 0);
-  }
-  return num;
-};
-console.log(addDigits(38));
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
 
-// Input: num = 38
-// Output: 2
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+
+var reverseString = function (s) {
+  return s.reverse();
+};
+console.log(reverseString(["h", "e", "l", "l", "o"]));
