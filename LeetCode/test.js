1f5def87a13abf6e596c4ab77e6d83761b233932
litakk <aazizaliev2403@gmail.com>
2025-11-05 15:39:53 +0500

    update

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,12 +1,28 @@
-// Input: s = ["h","e","l","l","o"]
-// Output: ["o","l","l","e","h"]
-
 /**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
+ * @param {string} s
+ * @return {string}
  */
+var reverseVowels = function (s) {
+  let seen = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+  let arr = s.split("");
+  let left = 0;
+  let right = s.length - 1;
 
-var reverseString = function (s) {
-  return s.reverse();
+  while (left > right) {
+    if (!seen.has(arr[left])) {
+      left++;
+      continue;
+    }
+
+    if (!seen.has(arr[right])) {
+      right--;
+      continue;
+    }
+
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+
+  return arr.join("");
 };
-console.log(reverseString(["h", "e", "l", "l", "o"]));
