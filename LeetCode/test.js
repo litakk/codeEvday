3d65a280d07b58d78cffe727f8480f8f66a90bb5
litakk <aazizaliev2403@gmail.com>
2025-11-05 14:16:15 +0500

    update

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,33 +1,14 @@
 /**
- * @param {string[]} operations
- * @return {number}
+ * @param {number} num
+ * @return {boolean}
  */
-var calPoints = function (operations) {
-  let total = [];
-
-  for (let i = 0; i < operations.length; i++) {
-    const op = operations[i];
-
-    if (op === "+") {
-      total.push(total[total.length - 1] + total[total.length - 2]);
-    } else if (op === "D") {
-      total.push(total[total.length - 1] * 2);
-    } else if (op === "C") {
-      total.pop();
-    } else {
-      total.push(parseInt(op));
+var isPerfectSquare = function (num) {
+  for (let i = 0; i < num; i++) {
+    if (i * i === num) {
+      return true;
     }
   }
-  return total.reduce((acc, cur) => acc + cur, 0);
+  return false;
 };
-
-console.log(calPoints(["5", "2", "C", "D", "+"]));
-
-// Input: ops = ["5","2","C","D","+"]
-// Output: 30
-
-// Input: ops = ["5","-2","4","C","D","9","+","+"]
-// Output: 27
-
-// Input: ops = ["1","C"]
-// Output: 0
+// 4 * 4 = 16
+console.log(isPerfectSquare(16));
